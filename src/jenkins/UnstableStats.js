0352daeeca8692f9797d8a07b0609bab601fc366
Bo Gotthardt <mail@gotthardt.bo>
2015-03-25 21:28:16 +0100

    Failures in table.

--- src/jenkins/UnstableStats.js
+++ src/jenkins/UnstableStats.js
@@ -1,7 +1,7 @@
 import React from "react";
 import _ from "lodash";
 import moment from "moment";
-import {Panel} from "react-bootstrap"
+import {Panel, Table} from "react-bootstrap"
 import Mixins from "../util/Mixins";
 import SubscribeMixin from "../flux/SubscribeMixin";
 
@@ -19,7 +19,7 @@ export default class UnstableStats extends React.Component {
         this.setState({testReports: this.props.integrationTests.getTestReports()});
     }
 
-    blah(getKey) {
+    sortAndFilter(getKey) {
         var count = {};
         _.forEach(this.state.testReports, (report) => {
             _.forEach(report.getFailedTests(), (failure) => {
@@ -44,37 +44,50 @@ export default class UnstableStats extends React.Component {
                 return b.name.localeCompare(a.name);
             }
         });
-        var interesting = sorted;/*_.filter(sorted, (item) => {
+        var interesting = _.filter(sorted, (item) => {
             return item.count > 1;
-        });*/
+        });
 
         return interesting;
     }
 
     renderTests() {
-        var x = this.blah((failure) => {
+        var failedTests = this.sortAndFilter((failure) => {
             return failure.file + " " + failure.name;
         });
 
-        return _.map(x, (item) => {
+        return _.map(failedTests, (item) => {
+            var fullKlass = item.name.substr(0, item.name.indexOf(" "));
+            var klass = fullKlass.substr(fullKlass.lastIndexOf(".") + 1);
+            var method = item.name.substr(item.name.indexOf(" "));
+            var klassLink = "https://github.com/Tradeshift/Integration-Test/tree/master/src/test/groovy/" + fullKlass.replace(/\./g, "/") + ".groovy";
             return (
-                <div key={item.name}>
-                    {item.name}: {item.count}
-                </div>
+                <tr key={item.name}>
+                    <td>
+                        <a href={klassLink} target="_blank">
+                            {klass}
+                        </a>
+                    </td>
+                    <td>{method}</td>
+                    <td>{item.count}</td>
+                </tr>
             )
         });
     }
 
-    renderSlaves() {
-        var x = this.blah((failure) => {
+    renderNodes() {
+        var nodes = this.sortAndFilter((failure) => {
             return failure.slave;
         });
 
-        return _.map(x, (item) => {
+        return _.map(nodes, (item) => {
             return (
-                <div key={item.name}>
-                    <a href={"/computer/slave-" + item.name} target="_blank">{item.name}</a>: {item.count}
-                </div>
+                <tr key={item.name}>
+                    <td>
+                        <a href={"/computer/slave-" + item.name} target="_blank">{item.name}</a>
+                    </td>
+                    <td>{item.count}</td>
+                </tr>
             )
         });
     }
@@ -82,11 +95,35 @@ export default class UnstableStats extends React.Component {
     render() {
         return (
             <div>
-                <Panel header="Failures by test">
-                    {this.renderTests()}
+                <Panel header={<h1>Failures by method</h1>}>
+                    <p>Counting failures from all builds there are data for, regardless of whether the test failure was correct. Methods with only a single failure are not shown.</p>
+                    <Table striped condensed hover>
+                        <thead>
+                        <tr>
+                            <th>Class</th>
+                            <th>Method</th>
+                            <th>Failures</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            {this.renderTests()}
+                        </tbody>
+                    </Table>
                 </Panel>
-                <Panel header="Failures by slave">
-                    {this.renderSlaves()}
+                <Panel header={<h1>Failures by node</h1>}>
+                    <p>Counting failures from all builds there are data for, regardless of how many builds the node has run. Nodes with only a single failure are not shown.</p>
+
+                    <Table striped condensed hover>
+                        <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Failures</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            {this.renderNodes()}
+                        </tbody>
+                    </Table>
                 </Panel>
             </div>
         );
