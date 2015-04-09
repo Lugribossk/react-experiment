ba43f409cb7e2472ecfa4ec1687c9279aca7daf2
Bo Gotthardt <mail@gotthardt.bo>
2015-04-09 16:49:07 +0200

    Improved parameter details.

--- src/jenkins/ui/ParameterDetails.js
+++ src/jenkins/ui/ParameterDetails.js
@@ -4,22 +4,41 @@ import GithubLink from "./GithubLink";
 import BuildUtils from "../build/BuildUtils";
 
 export default class ParameterDetails extends React.Component {
+    renderAllBranch() {
+        var onlyMaster = true;
+        _.each(BuildUtils.getRepoBranches(this.props.parameters), (branch) => {
+            if (branch && branch !== "master") {
+                onlyMaster = false;
+            }
+        });
+
+        if (this.props.parameters.BRANCH !== "master" || onlyMaster) {
+            return <span>All repos: {this.props.parameters.BRANCH}</span>
+        }
+    }
+
     renderBranches() {
         return _.map(BuildUtils.getRepoBranches(this.props.parameters), (branch, repo) => {
-            if (branch && branch !== "master") {
+            var nonMasterBranch = (branch && branch !== "master");
+            var itForNonMasterAll = (repo === "Integration-Test" && this.props.parameters.BRANCH !== "master");
+            if (nonMasterBranch || itForNonMasterAll) {
                 return <GithubLink key={repo} owner="Tradeshift" repo={repo} branch={branch} />;
             }
         });
     }
 
+    renderPackage() {
+        if (this.props.parameters.PACKAGE_PATH !== "com/tradeshift") {
+            return <span>Only running tests in package: {this.props.parameters.PACKAGE_PATH}</span>
+        }
+    }
+
     render() {
         return (
             <div>
-                {this.props.parameters.BRANCH !== "master" &&
-                <span>All repos: {this.props.parameters.BRANCH}</span>}
+                {this.renderAllBranch()}
                 {this.renderBranches()}
-                {this.props.parameters.PACKAGE_PATH !== "com/tradeshift" &&
-                <span>Only running tests in package: {this.props.parameters.PACKAGE_PATH}</span>}
+                {this.renderPackage()}
             </div>
         )
     }
